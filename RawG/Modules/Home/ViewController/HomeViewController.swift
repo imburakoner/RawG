3458adb7e279230c8aa812211ed076d6381579c8
Burak ÖNER <imburakoner@gmail.com>
2021-11-14 13:40:23 +0300

    Application created. gitignore and swiftlint added.

---
+++ RawG/Modules/Home/ViewController/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  RawG
+//
+//  Created by Burak Oner on 14.11.2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
